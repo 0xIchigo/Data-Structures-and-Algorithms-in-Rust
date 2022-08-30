3ae4c082686b48d42642133236138d97784b95da
0xIchigo <0xIchigo@protonmail.com>
2022-08-30 15:24:56 -0400

    Initial commit

---
+++ src/linear_search.rs
@@ -0,0 +1,24 @@
+pub fn linear_search<T: PartialEq>(item: &T, array: &[T]) -> i32{
+    let mut index_position = -1;
+
+    for (index, data) in array.iter().enumerate() {
+        if item == data {
+            index_position = index as i32;
+            return index_position;
+        }
+    }
+
+    index_position
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_linear_search() {
+        assert_eq!(linear_search(&12, &vec![12, 3, 4, 36, 777]), 0);
+        assert_eq!(linear_search(&100, &vec![12, 3, 4, 36, 777]), -1);
+        assert_eq!(linear_search(&"Apple", &vec!["Orange", "Pear", "Apple", "Grape", "Banana"]), 2);
+    }
+}
